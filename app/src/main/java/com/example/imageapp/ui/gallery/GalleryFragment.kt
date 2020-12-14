4f729db3f3b743abd8b3cadd5f7009ed379a6ae6
Daria <kh.d.m@yandex.ru>
2020-12-14 11:58:34 +0300

    comments

--- app/src/main/java/com/example/imageapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imageapp/ui/gallery/GalleryFragment.kt
@@ -22,6 +22,8 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery),
 
     private val viewModel by viewModels<GalleryViewModel>()
 
+    // code that kills binding when a fragment kills to avoid the situation where
+    // the activity is dead but there is still fragment binding which would cause application crash
     private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
 
@@ -33,6 +35,8 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery),
         val adapter = UnsplashPhotoAdapter(this)
 
         // region loadstate header/footer (progress bar on loading + retry)
+        // recyclerView is a layout container
+        // that also handles deleting currently unused items to save memory
         binding.apply {
             recyclerView.setHasFixedSize(true)
             recyclerView.itemAnimator = null
